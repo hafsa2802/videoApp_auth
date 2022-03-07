6b177400b8a291cc72b9911fbaed667bafe49d70
hafsamehdioui <mehdiouihafsa@gmail.com>
2022-03-07 23:28:00 +0100

    added login to backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors =require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
+const jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json()) 
@@ -30,7 +31,13 @@ app.post('/api/login', async (req,res) => {
             password: req.body.password,
     })
     if(user) {
-        return res.json({status: 'ok', user: true })
+
+        const token = jwt.sign({
+            name: user.name,
+            email: user.email,
+
+        } , 'hafsa123')
+        return res.json({status: 'ok', user: token })
     }
     else {
         return res.json({status: 'ok', user: false })
